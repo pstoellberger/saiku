6e88a1d31465540316011acf8983114f78f4191e
pstoellberger <pstoellberger@gmail.com>
2015-05-23 16:58:48 +0200

    include saiku-query

--- buildall.sh
+++ buildall.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+cd saiku-query
+git checkout master
+git pull origin master
+mvn install
+cd ..
 cd saiku-core
-mvn clean install  -DskipTests 
+mvn clean install  
 cd ..
 cd saiku-webapp
 mvn clean install
@@ -9,8 +14,8 @@ git submodule init
 git submodule update
 cd saiku-ui
 git pull
-git checkout saiku3 
-git pull origin saiku3
+git checkout dev 
+git pull origin dev
 mvn clean package install:install-file -Dfile=target/saiku-ui-3.0-PSTOELLBERGER-SNAPSHOT.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=3.0-PSTOELLBERGER-SNAPSHOT -Dpackaging=war
 cd ../saiku-server
 mvn clean package
