b64411c330a325407009766c2c3a908df1588c0c
Paul Stoellberger <pstoellberger@gmail.com>
2015-11-05 21:06:15 +0100

    remove old stuff, remove impl

--- saiku-core/saiku-web/src/main/java/org/saiku/web/connection/SecurityAwareConnectionManager.java
+++ saiku-core/saiku-web/src/main/java/org/saiku/web/connection/SecurityAwareConnectionManager.java
@@ -13,7 +13,7 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
-package org.saiku.web.impl;
+package org.saiku.web.connection;
 
 import java.io.Serializable;
 import java.sql.Connection;
