1620277636af3126ee9608952c8a83a5eb55bf3d
Paul Stoellberger <pstoellberger@gmail.com>
2015-11-05 21:25:03 +0100

    fix saiku ui artifact build, build script

--- buildall.sh
+++ buildall.sh
@@ -4,24 +4,13 @@ git submodule update
 cd saiku-query
 git checkout master
 git pull origin master
-mvn install
 cd ..
-cd saiku-core
-mvn clean install  
-cd ..
-cd saiku-webapp
-mvn clean install
-cd ..
-git submodule init
-git submodule update
 cd saiku-ui
 git pull
 git checkout dev 
 git pull origin dev
-mvn clean package install:install-file -Dfile=target/saiku-ui-3.0-PSTOELLBERGER-SNAPSHOT.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=3.0-PSTOELLBERGER-SNAPSHOT -Dpackaging=war
-cd ../saiku-server
+cd ..
+echo "Building Everything"
 mvn clean package
-#cd ../saiku-bi-platform-plugin
+#cd ../saiku-bi-platform-plugin-p5
 #mvn clean package
-cd ../saiku-bi-platform-plugin-p5
-mvn clean package
